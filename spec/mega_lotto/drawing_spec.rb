5f3e846fc07ecef847499cefdc7a7966f7f0699a
Rodolfo Spalenza <rodolfo.spalenza@gmail.com>
2014-12-17 13:00:46 -0200

    create drawn method

---
+++ spec/mega_lotto/drawing_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+module MegaLotto
+  describe Drawing do
+    describe "#draw" do
+      let(:draw) { MegaLotto::Drawing.new.draw }
+      it "return an array" do
+        expect(draw).to be_a(Array)
+      end
+
+      it "return an array with 6 elements" do
+        expect(draw.size).to eq(5)
+      end
+
+      it "each element is an integer" do
+        draw.each { |number| expect(number).to be_a(Integer) }
+      end
+
+      it "each element is less than 60" do
+        draw.each { |number| expect(number).to be < 60 }
+      end
+    end
+  end
+end
